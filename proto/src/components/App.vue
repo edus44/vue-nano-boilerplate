fa0c03a59b3931f277f11f695095aa3feabd7607
Eduardo <edu@edumac.local>
2017-01-22 18:59:04 +0100

    initial

---
+++ proto/src/components/App.vue
@@ -0,0 +1,25 @@
+<template>
+    <div>
+        the app
+        <MyButton>enter</MyButton>
+    </div>
+</template>
+
+<script>
+    import MyButton from './MyButton.vue'
+
+    export default {
+        name: 'App',
+        created(){
+            console.log('created')
+        },
+        components: {MyButton}
+    }
+</script>
+
+<style lang="less">
+@bg:#ccc;
+body{
+    background-color:@bg;
+}
+</style>
---
+++ proto/src/components/MyButton.vue
@@ -0,0 +1,13 @@
+<template>
+    <button>
+        <slot></slot>
+    </button>
+</template>
+
+<style scoped>
+button{
+    background-color: #bada55;
+    border:none;
+    border-radius: 4px;
+}
+</style>
